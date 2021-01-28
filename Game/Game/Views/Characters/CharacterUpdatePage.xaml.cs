7a9e74fea38fc37fcd97e25adf87445325074cd5
Mike Koenig <koenigm@seattleu.edu>
2021-01-27 17:35:34 -0800

    Added Character to Tile on CharacterUpdatePage.xaml.cs

--- Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
+++ Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
@@ -30,7 +30,7 @@ namespace Game.Views
 
             BindingContext = this.ViewModel = data;
 
-            this.ViewModel.Title = "Update " + data.Title;
+            this.ViewModel.Title = "Character Update " + data.Title;
 
             //Need to make the SelectedItem a string, so it can select the correct item.
             LocationPicker.SelectedItem = data.Data.Location.ToString();
