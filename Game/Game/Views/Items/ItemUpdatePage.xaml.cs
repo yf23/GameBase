7efa4268cb6583b15dcad0dd7beeb2dbdced22c5
Mike Koenig <koenigm@seattleu.edu>
2021-02-03 15:15:32 -0800

    Change to use the ToMessage Friendly name in ItemUpdatePage.xaml, and ItemUpdatePage.xaml.cs

--- Game/Game/Views/Items/ItemUpdatePage.xaml.cs
+++ Game/Game/Views/Items/ItemUpdatePage.xaml.cs
@@ -33,7 +33,7 @@ namespace Game.Views
             this.ViewModel.Title = "Update " + data.Title;
 
             //Need to make the SelectedItem a string, so it can select the correct item.
-            LocationPicker.SelectedItem = data.Data.Location.ToString();
+            LocationPicker.SelectedItem = data.Data.Location.ToMessage();
             AttributePicker.SelectedItem = data.Data.Attribute.ToString();
         }
 
