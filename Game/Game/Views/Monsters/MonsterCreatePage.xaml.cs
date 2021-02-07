4b97b34bdb955936650a2ef1660a486974761680
Mike Koenig <koenigm@seattleu.edu>
2021-02-07 12:20:42 -0800

    Added MonsterCreatePage.xaml and MonsterCreatePage.xaml.cs to project

---
+++ Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.ComponentModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Game.Models;
+using Game.ViewModels;
+using Game.GameRules;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// Create Character
+    /// </summary>
+    [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
+    public partial class MonsterCreatePage : ContentPage
+    {
+        // The Character to create
+        public GenericViewModel<CharacterModel> ViewModel = new GenericViewModel<CharacterModel>();
+
+        // Hold the current location selected
+        public ItemLocationEnum PopupLocationEnum = ItemLocationEnum.Unknown;
+
+        // Empty Constructor for UTs
+        public MonsterCreatePage(bool UnitTest) { }
+
+        /// <summary>
+        /// Constructor for Create makes a new model
+        /// </summary>
+        public MonsterCreatePage()
+        {
+            InitializeComponent();
+
+            this.ViewModel.Data = RandomPlayerHelper.GetRandomCharacter(1);
+            this.ViewModel.Title = this.ViewModel.Data.Name;
+
+            BindingContext = ViewModel;
+        }
+
+        /// <summary>
+        /// Save by calling for Create
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Save_Clicked(object sender, EventArgs e)
+        {
+            // If the image in the data box is empty, use the default one..
+            if (string.IsNullOrEmpty(ViewModel.Data.ImageURI))
+            {
+                ViewModel.Data.ImageURI = new CharacterModel().ImageURI;
+            }
+
+            MessagingCenter.Send(this, "Create", ViewModel.Data);
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Cancel the Create
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Randomize Character Values and Items
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void RandomButton_Clicked(object sender, EventArgs e)
+        {
+            BindingContext = null;
+
+            this.ViewModel.Data = RandomPlayerHelper.GetRandomCharacter(1);
+            this.ViewModel.Title = this.ViewModel.Data.Name;
+
+            BindingContext = ViewModel;
+
+            return;
+        }
+    }
+}
