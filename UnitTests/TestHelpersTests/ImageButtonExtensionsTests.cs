231a24cb9f9497f66575064b736a439427e2b4e5
Mike Koenig <koenigm@seattleu.edu>
2021-02-17 13:11:07 -0800

    Added UT Coverage for ImageButtonExtensionsTests.cs

--- UnitTests/TestHelpersTests/ImageButtonExtensionsTests.cs
+++ UnitTests/TestHelpersTests/ImageButtonExtensionsTests.cs
@@ -4,6 +4,10 @@ using Game;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Mocks;
+using Game.ViewModels;
+using Game.Models;
+using Game.Views;
+using System.Linq;
 
 namespace UnitTests.TestHelpers
 {
@@ -60,5 +64,41 @@ namespace UnitTests.TestHelpers
             // Assert
             Assert.IsTrue(true);    // Got here
         }
+
+
+        [Test]
+        public void PerformClick_Valid_Click_Button_Should_Open_Popup()
+        {
+            // Arrange
+            
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            var page = new MonsterUpdatePage(new GenericViewModel<MonsterModel>(new MonsterModel()));
+
+            page.ViewModel.Data.UniqueItem = ItemIndexViewModel.Instance.GetDefaultItemId(ItemLocationEnum.Head);
+
+            var stackView = page.GetItemToDisplay();
+
+            ImageButton imageButtonView = new ImageButton();
+
+            foreach (View i in ((StackLayout)stackView).Children.Where(x => x.GetType() == typeof(ImageButton)))
+            {
+                imageButtonView = (ImageButton)i;
+            }
+
+            // Act
+            imageButtonView.PerformClick();
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
     }
 }
