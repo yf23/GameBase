851868ec66ca094acee783b569888238aa1ca76a
Mike Koenig <koenigm@seattleu.edu>
2021-02-16 20:27:22 -0800

    Added coverage for ItemLocationEnum convertor in ItemLocationEnumHelperTests.cs

--- UnitTests/Helpers/ItemLocationEnumHelperTests.cs
+++ UnitTests/Helpers/ItemLocationEnumHelperTests.cs
@@ -1,10 +1,10 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Linq;
 
-using Game.Services;
+using NUnit.Framework;
+
 using Game.Models;
 using Game.Helpers;
-using System;
-using System.Linq;
 
 namespace UnitTests.Helpers
 {
@@ -264,5 +264,118 @@ namespace UnitTests.Helpers
             // Assert
             Assert.AreEqual(Expected, Actual, TestContext.CurrentContext.Test.Name);
         }
+
+        #region ItemLocationEnumConverter
+
+        [Test]
+        public void ItemLocationEnumConvert_String_Should_Pass()
+        {
+            var myConverter = new ItemLocationEnumConverter();
+
+            var myObject = "Feet";
+            var Result = myConverter.Convert(myObject, typeof(ItemLocationEnum), null, null);
+            var Expected = ItemLocationEnum.Feet.ToMessage();
+
+            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+        }
+
+        [Test]
+        public void ItemLocationEnumConvert_Enum_Should_Pass()
+        {
+            var myConverter = new ItemLocationEnumConverter();
+
+            var myObject = ItemLocationEnum.Feet;
+            var Result = myConverter.Convert(myObject, null, null, null);
+            var Expected = "Feet";
+
+            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+        }
+
+        [Test]
+        public void ItemLocationEnumConvert_Other_Should_Skip()
+        {
+            var myConverter = new ItemLocationEnumConverter();
+
+            var myObject = new ItemModel();
+            var Result = myConverter.Convert(myObject, null, null, null);
+            var Expected = 0;
+
+            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+        }
+
+        // Convert Back
+        [Test]
+        public void IntEnumConvertBack_Should_Skip()
+        {
+            var myConverter = new IntEnumConverter();
+
+            var myObject = "Bogus";
+            var Result = myConverter.ConvertBack(myObject, null, null, null);
+            var Expected = 0;
+
+            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+        }
+
+        // Convert Back
+        [Test]
+        public void IntEnumConvertBack_Int_Should_Pass()
+        {
+            var myConverter = new IntEnumConverter();
+
+            int myObject = 40;
+            var Result = myConverter.ConvertBack(myObject, typeof(ItemLocationEnum), null, null);
+            var Expected = "Feet";
+
+            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+        }
+
+        [Test]
+        public void ItemLocationEnumConvertBack_String_Should_Pass()
+        {
+            var myConverter = new ItemLocationEnumConverter();
+
+            var myObject = "Feet";
+            var Result = myConverter.ConvertBack(myObject, typeof(ItemLocationEnum), null, null);
+            var Expected = ItemLocationEnum.Feet;
+
+            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+        }
+
+        [Test]
+        public void ItemLocationEnumConvertBack_Enum_Should_Skip()
+        {
+            var myConverter = new ItemLocationEnumConverter();
+
+            var myObject = ItemLocationEnum.Feet;
+            var Result = myConverter.ConvertBack(myObject, null, null, null);
+            var Expected = 0;
+
+            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+        }
+
+        [Test]
+        public void ItemLocationEnumConvertBack_Other_Should_Skip()
+        {
+            var myConverter = new ItemLocationEnumConverter();
+
+            var myObject = new ItemModel();
+            var Result = myConverter.ConvertBack(myObject, null, null, null);
+            var Expected = 0;
+
+            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+        }
+
+        [Test]
+        public void ItemLocationEnumConvertBack_Int_Should_Pass()
+        {
+            var myConverter = new ItemLocationEnumConverter();
+
+            int myObject = 40;
+            var Result = myConverter.ConvertBack(myObject, typeof(ItemLocationEnum), null, null);
+            var Expected = "Feet";
+
+            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+        }
+        #endregion ItemLocationEnumConverter
     }
 }
