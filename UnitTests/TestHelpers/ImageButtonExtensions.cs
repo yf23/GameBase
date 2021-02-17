0273694d4e661f3c1990116de9c79f0584bacee0
Mike Koenig <koenigm@seattleu.edu>
2021-02-17 13:10:53 -0800

    Commented out code not reachable in ImageButtonExtensions.cs

--- UnitTests/TestHelpers/ImageButtonExtensions.cs
+++ UnitTests/TestHelpers/ImageButtonExtensions.cs
@@ -23,16 +23,16 @@ public static class ImageButtonExtensions
         // 1.) Raise the Click-event
         sourceButton.RaiseEventViaReflection(nameof(sourceButton.Clicked), EventArgs.Empty);
 
-        // 2.) Execute the command, if bound and can be executed
-        ICommand boundCommand = sourceButton.Command;
-        if (boundCommand != null)
-        {
-            object parameter = sourceButton.CommandParameter;
-            if (boundCommand.CanExecute(parameter) == true)
-            {
-                boundCommand.Execute(parameter);
-            }
-        }
+        //// 2.) Execute the command, if bound and can be executed
+        //ICommand boundCommand = sourceButton.Command;
+        //if (boundCommand != null)
+        //{
+        //    object parameter = sourceButton.CommandParameter;
+        //    if (boundCommand.CanExecute(parameter) == true)
+        //    {
+        //        boundCommand.Execute(parameter);
+        //    }
+        //}
     }
 
     /// <summary>
@@ -52,7 +52,8 @@ public static class ImageButtonExtensions
 
         foreach (var handler in eventDelegate.GetInvocationList())
         {
-            handler.GetMethodInfo()?.Invoke(handler.Target, new object[] { source, eventArgs });
+            handler.GetMethodInfo().Invoke(handler.Target, new object[] { source, eventArgs });
+            // handler.GetMethodInfo()?.Invoke(handler.Target, new object[] { source, eventArgs });
         }
     }
 }
