7f00776d8b2cad3fbeca4f5980ab798f965fea91
Mike Koenig <koenigm@seattleu.edu>
2021-02-16 21:49:58 -0800

    Added UT coverage to MonsterCreatePageTests.cs

--- UnitTests/Views/Monsters/MonstersCreatePageTests.cs
+++ UnitTests/Views/Monsters/MonstersCreatePageTests.cs
@@ -131,7 +131,7 @@ namespace UnitTests.Views
         //    // Assert
         //    Assert.IsTrue(true); // Got to here, so it happened...
         //}
-        
+
         //[Test]
         //public void MonsterCreatePage_Speed_OnStepperValueChanged_Default_Should_Pass()
         //{
@@ -297,5 +297,20 @@ namespace UnitTests.Views
         //    // Assert
         //    Assert.IsTrue(true); // Got to here, so it happened...
         //}
+
+        [Test]
+        public void MonsterCreatePage_RandomButton_Clicked_Vaid_Should_Pass()
+        {
+            // Arrange
+            page.ViewModel.Data.ImageURI = null;
+
+            // Act
+            page.RandomButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
