fc57d129a8029a83b6198e57d4764a1a0d238976
Mike Koenig <koenigm@seattleu.edu>
2021-02-25 13:01:56 -0800

    Splash screen and name changes for the game.  Changes to UWP project

--- Game/Game.UWP/Properties/AssemblyInfo.cs
+++ Game/Game.UWP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Elves vs Trolls")]
-[assembly: AssemblyDescription("Mike's Cool Game")]
+[assembly: AssemblyTitle("Trolls Rule")]
+[assembly: AssemblyDescription("Great Dungeon game by Mike Software")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SU")]
-[assembly: AssemblyProduct("Elves vs Trolls")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCompany("SeattleU 5250")]
+[assembly: AssemblyProduct("Trolls Rule Game")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.2")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
