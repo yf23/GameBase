511a7e66da69af441fff459a6ee0a1d7dc23c30f
Mike Koenig <koenigm@seattleu.edu>
2021-02-26 20:51:54 -0800

    Updated to use troll in MonsterModel.cs

--- Game/Game/Models/MonsterModel.cs
+++ Game/Game/Models/MonsterModel.cs
@@ -23,7 +23,7 @@ namespace Game.Models
             Attack = 1;
             Difficulty = DifficultyEnum.Average;
             UniqueItem = null;
-            ImageURI = "item.png";
+            ImageURI = "troll.png";
             ExperienceTotal = 0;
             ExperienceRemaining = LevelTableHelper.LevelDetailsList[Level + 1].Experience - 1;
 
